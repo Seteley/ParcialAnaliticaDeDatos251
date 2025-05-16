f39c739d58eb2acb5d2d43f70617f45e6d44e6ab
Martin Zapana <martin.zapana.b@uni.pe>
2025-05-16 00:14:19 -0500

    zona horaria

--- API/api.py
+++ API/api.py
@@ -2,11 +2,15 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS  # Importa CORS
 from unofficial_livecounts_api.twitter import TwitterAgent
 from datetime import datetime
+import pytz  # Importamos pytz para manejar zonas horarias
 
 app = Flask(__name__)
 
 # Habilitar CORS para todas las rutas
 CORS(app)
+
+# Definir la zona horaria de Lima, Perú (UTC-5)
+zona_horaria_lima = pytz.timezone('America/Lima')
 
 @app.route('/')
 def index():
@@ -25,9 +29,12 @@ def monitorear_seguidores_usuario(nombre_usuario):
         tweets = metricas.tweet_count
         following = metricas.following_count
 
+        # Obtener la hora actual en UTC-5
+        hora_lima = datetime.now(zona_horaria_lima)
+
         # Crear la respuesta en formato JSON
         respuesta = {
-            "hora": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "hora": hora_lima.strftime('%d/%m/%Y %H:%M:%S'),  # Formato: Día/Mes/Año Hora:Minuto:Segundo
             "usuario": f"@{nombre_usuario}",
             "seguidores": seguidores,
             "tweets": tweets,
